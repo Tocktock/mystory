45feb9a3f77bfc5c0e8e57bac815acfbdef5cb4a
manyang <qnfmtm666@naver.comm>
2021-09-08 23:01:26 +0900

    글쓰기 방 파일 생성

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,13 @@
 import NavBar from "../src/components/navigation/NavBar";
 import Welcome from "../src/components/landing/welcome/Welcome";
-import CategoryBar from "../src/components/landing/category/CategoryBar";
 import NewPost from "../src/components/landing/newpost/NewPost";
+
 export default function Home() {
-  return (
-    <>
-      <NavBar></NavBar>
-      <Welcome></Welcome>
-      <NewPost></NewPost>
-    </>
-  );
+    return (
+        <>
+            <NavBar/>
+            <Welcome/>
+            <NewPost/>
+        </>
+    );
 }
---
+++ pages/write/index.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+    return (
+        <>
+            <div> now you are a honor thee</div>
+        </>
+    );
+}
