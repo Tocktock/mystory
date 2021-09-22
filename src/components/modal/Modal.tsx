bfc4e8e12542e5920ea1e525a7157036ae20fcec
Ji Young Park <qnfmtm666@naver.com>
2021-09-22 13:45:24 +0900

    chaning

---
+++ src/components/modal/Modal.tsx
@@ -0,0 +1,39 @@
+import { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { RootState } from "../../features/rootReducer";
+
+interface Props {}
+
+const ModalAuth: React.FC<Props> = (props) => {
+  // for test
+  const dispatch = useDispatch();
+
+  const { modalDisplay } = useSelector((state: RootState) => state.modalState);
+
+  return (
+    <div
+      className={
+        "flex fixed inset-0 overflow-y-auto z-20 justify-center items-center min-h-full text-center " +
+        modalDisplay
+      }
+    >
+      {/* background overlay */}
+      <div className="fixed inset-0 transition-opacity" aria-hidden="true">
+        <div className="absolute inset-0 bg-gray-500 opacity-75"></div>
+      </div>
+      {/* trick the browser into centering the modal contents */}
+      <span
+        className="hidden sm:inline-block sm:align-middle sm:h-screen"
+        aria-hidden="true"
+      ></span>
+      <div
+        className="relative flex align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-xl sm:w-full "
+        role="dialog"
+        aria-modal="true"
+        aria-labelledby="modal-headline"
+      ></div>
+    </div>
+  );
+};
+
+export default ModalAuth;
--- src/components/writeSpace/WriteBody.tsx
+++ src/components/writeSpace/WriteBody.tsx
@@ -4,19 +4,26 @@ import { updateContent } from "../../features/markdown/markdownSlice";
 
 const WriteBody = () => {
   const dispatch = useDispatch();
-
+  const textRef = useRef<HTMLTextAreaElement>(null);
+  const enterCheck = (e) => {
+    if (e.key == "Enter") {
+      textRef.current.value =
+        textRef.current.value.substring(0, textRef.current.selectionStart) +
+        "  " +
+        textRef.current.value.substring(textRef.current.selectionStart);
+    }
+  };
   const setPreveiw = (e) => {
-    dispatch(updateContent(e.target.innerText));
+    dispatch(updateContent(textRef.current.value));
   };
 
   return (
     <div className="h-3/4 w-full bg-white border-l-2 border-r-2 overflow-auto">
-      <div
-        contentEditable
+      <textarea
+        ref={textRef}
         placeholder="enter some text"
         className="w-full h-full from-current focus:outline-none p-3"
-        onKeyUp={setPreveiw}
-      ></div>
+      ></textarea>
     </div>
   );
 };
